e0167ca3e49d8d72aaac3d7a5148a104a923ef00
fernangb <gfsouza@poli.ufrj.br>
2020-08-10 19:34:02 -0300

    Login com autenticação

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,7 +3,7 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import { Container, Content, Background, AnimationContainer } from './styles';
 import logoImg from '../../assets/logo.svg';
 
@@ -24,6 +24,8 @@ const SignIn: React.FC = () => {
 
   const {signIn } = useAuth();
   const {addToast} = useToast();
+  const history = useHistory();
+
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
@@ -45,6 +47,8 @@ const SignIn: React.FC = () => {
           email: data.email,
           password: data.password,
         });
+
+        history.push('/dashboard');
       } catch (err) {
         if (err instanceof Yup.ValidationError){
           const errors = getValidationErrors(err);
@@ -58,10 +62,9 @@ const SignIn: React.FC = () => {
           title :'Erro na autenticação',
           description: 'Ocorreu um erro ao fazer o login. Cheque as suas credenciais.',
         });
-
       }
     },
-    [signIn, addToast],
+    [signIn, addToast, history],
   );
   return (
     <Container>
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,19 +3,28 @@ import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import { Container, Content, Background, AnimationContainer } from './styles';
 import logoImg from '../../assets/logo.svg';
-
+import api from '../../services/api';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-
 import getValidationErrors from '../../utils/getValidationErrors';
+import {useToast} from '../../hooks/toast';
+
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
 
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const {addToast} = useToast();
+  const history = useHistory();
 
-  const handleSubmit = useCallback(async (data: object) => {
+  const handleSubmit = useCallback(async (data: SignUpFormData) => {
     formRef.current?.setErrors({});
 
     try {
@@ -30,11 +39,33 @@ const SignUp: React.FC = () => {
       await schema.validate(data, {
         abortEarly: false,
       });
+
+      api.post('/users', data);
+
+      history.push('/');
+
+      addToast({
+        type: 'success',
+        title: 'Cadastro realizado!',
+        description: 'Você já pode fazer seu logon no GoBarber!'
+      });
+
+
     } catch (err) {
-      const errors = getValidationErrors(err);
-      formRef.current?.setErrors(errors);
+      if (err instanceof Yup.ValidationError){
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title :'Erro no cadastro',
+          description: 'Ocorreu um erro ao fazer o cadastro. Tente novamente.',
+        });
     }
-  }, []);
+  }, [addToast, history]);
 
   return (
     <Container>
